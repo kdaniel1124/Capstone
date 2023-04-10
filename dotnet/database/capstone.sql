6451db8b086fc3b41597d501abde495667ef25ce
Matt Eland <matt.eland@gmail.com>
2023-04-10 11:47:14 -0400

    Revert "Modifications for Azure SQL"
    This reverts commit c07dc56e540e02b9be6765b9f117ad7266a8de69.

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -1,3 +1,19 @@
+USE master
+GO
+
+-- Drop database if it exists
+IF DB_ID('final_capstone') IS NOT NULL
+BEGIN
+	ALTER DATABASE final_capstone SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE final_capstone;
+END
+
+CREATE DATABASE final_capstone
+GO
+
+USE final_capstone
+GO
+
 -- Create Tables
 CREATE TABLE users (
 	user_id int IDENTITY(1,1) NOT NULL,
