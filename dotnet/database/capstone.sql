4bf30f203853b83981954b772ebc4c9edb029861
Daniel Kennelly <kennellydanny@gmail.com>
2023-04-17 09:12:17 -0400

    pulling beginning of day 4/17/23

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -83,7 +83,7 @@ examples (title, code_language, code, approved )
 VALUES
 ('How to add comments', 'C#',
 'For a single line comment do //
- For a mult line comment do /* at the beginning and */ at the end
+ For a multi line comment do /* at the beginning and */ at the end
  For a To Do list do // TODO:', 1)
 
  INSERT INTO
